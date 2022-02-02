181c724186c6d18439e7a197e55469a67b37f067
Yan3330 <korolevskijan@gmail.com>
2022-02-02 21:01:01 +0300

    +корень и квадрат

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ let btnPlus = document.querySelector('#btn_plus')
 let btnMinus = document.querySelector('#btn_minus')
 let btnMultiply = document.querySelector('#btn_multiply')
 let btnDevedet = document.querySelector('#btn_divedet')
+let btnExp1 = document.querySelector('#btn_exp1')
+let btnExp2 = document.querySelector('#btn_exp2')
+let btnRoot1 = document.querySelector('#btn_root1')
+let btnRoot2 = document.querySelector('#btn_root2')
 let result = document.querySelector('.result')
 
 btnPlus.onclick = function () {
@@ -28,4 +32,24 @@ btnDevedet.onclick = function () {
         let sum = firstNumber.value*1 / secondNumber.value*1
     result.textContent = sum
     }
+}
+
+btnExp1.onclick = function () {
+    let exp = Math.pow(firstNumber.value*1, 2)
+    result.textContent = exp
+}
+
+btnExp2.onclick = function () {
+    let exp = Math.pow(secondNumber.value*1, 2)
+    result.textContent = exp
+}
+
+btnRoot1.onclick = function () {
+    let root = Math.sqrt(firstNumber.value*1)
+    result.textContent = root
+}
+
+btnRoot2.onclick = function () {
+    let root = Math.sqrt(secondNumber.value*1)
+    result.textContent = root
 }
